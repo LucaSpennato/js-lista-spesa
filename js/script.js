b6b51108cb490ffb3e15300bd586eb9f9ae47344
LucaSpennato <luca.spennato@gmail.com>
2022-06-21 18:26:44 +0200

    while added in btn, now add listitem

--- js/script.js
+++ js/script.js
@@ -10,32 +10,37 @@
 
 let shoppingList = ['pane', 'formaggini', 'uova', 'salame', 'verdurine', 'cioccolato', 'banane']
 
+let shoppingListAdded = [];
+
 let inputElement;
+
+let listWrapper = document.querySelector('.list_wrapper');
 
 
 const addBtn = document.getElementById('addbtn');
 
-
 addBtn.addEventListener('click', function(){
+
+    document.querySelector('.list_wrapper').innerHTML = '';
 
     inputElement = document.getElementById('add_shopping_items').value;
     console.log(inputElement);
+    shoppingList.push(inputElement);
+
+    let i = 0;
+
+    while (i < shoppingList.length) {
+        console.log(shoppingList[i]);
+    
+        let shoppingItem = document.createElement('li');
+        shoppingItem.innerHTML = shoppingList[i];
+        shoppingItem.classList.add('list-group-item');
+    
+        listWrapper.append(shoppingItem);
+    
+        i++
+    }
+    
+
 
 })
-
-let listWrapper = document.querySelector('.list_wrapper');
-console.log(listWrapper);
-
-let i = 0;
-
-while (i < shoppingList.length) {
-    console.log(shoppingList[i]);
-
-    let shoppingItem = document.createElement('li');
-    shoppingItem.innerHTML = shoppingList[i];
-    shoppingItem.classList.add('list-group-item');
-
-    listWrapper.append(shoppingItem);
-
-    i++
-}
